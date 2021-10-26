7b106781597ad427e6c9d1d4b928c277afa7a75a
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-10-27 02:19:39 +0300

    not finish

--- src/main/java/com/example/songr1/Album.java
+++ src/main/java/com/example/songr1/Album.java
@@ -1,29 +1,33 @@
 package com.example.songr1;
 
-import org.hibernate.annotations.Entity;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Album {
-    private   String title;
-    private  String artist;
-    private  String imageUrl;
-    private   int songCount;
-    private  int length;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private   String title;
+    private  String artist;
+    private  String image;
+    private   int count;
+    private  int length;
 
 
 
-    public Album(String title, String artist, String imageUrl, int songCount, int length) {
+    public Album(){
+
+    }
+
+    public Album(String title, String artist, String image, int count, int length) {
         this.title = title;
         this.artist = artist;
-        this.imageUrl=imageUrl;
-        this.songCount = songCount;
+        this.image=image;
+        this.count = count;
         this.length = length;
     }
 
@@ -36,14 +40,38 @@ public class Album {
     }
 
     public String getImageUrl() {
-        return imageUrl;
+        return image;
     }
 
     public int getSongCount() {
-        return songCount;
+        return count;
     }
 
     public int getLength() {
         return length;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setImageUrl(String image) {
+        this.image = image;
+    }
+
+    public void setSongCount(int count) {
+        this.count = count;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
     }
 }
